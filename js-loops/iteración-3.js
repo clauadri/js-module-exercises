c6651c211f08b78136e36fff8ab2d7ab279a3d8f
clauadri <adripa1111@gmail.com>
2022-10-07 13:14:47 +0200

    FEAT:exercise-3-loops

---
+++ js-loops/iteración-3.js
@@ -0,0 +1,10 @@
+// Usa un bucle forof para recorrer todos los destinos del array. 
+// Imprime en un ***console.log*** sus valores.
+
+// Puedes usar este array:
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (const recorrer of placesToTravel) {
+    console.log('Voy a recorrer', recorrer);
+}
